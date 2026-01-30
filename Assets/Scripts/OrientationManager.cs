74cc126499b1f83d2ea254ce722ef8bd8a9fa67a
Talha DOĞAN <109479115+Talha-Dogan@users.noreply.github.com>
2026-01-31 02:04:43 +0300

    Initial commit

---
+++ Assets/Scripts/OrientationManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class OrientationManager : MonoBehaviour
+{
+    // Singleton (Opsiyonel, erişim kolaylığı için kalsın)
+    public static OrientationManager Instance;
+
+    private bool _lastIsPortrait;
+    private bool _firstRun = true;
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); }
+    }
+
+    void Start()
+    {
+        // Oyun açıldığında mevcut durumu kontrol et ve uygula
+        CheckOrientationAndRefresh();
+    }
+
+    void Update()
+    {
+        // Her karede ekran oranını kontrol ediyoruz.
+        // Eğer oran değişirse UI'ı güncelleyeceğiz.
+        CheckOrientationAndRefresh();
+    }
+
+    private void CheckOrientationAndRefresh()
+    {
+        // Ekranın yüksekliği genişliğinden büyükse DİKEY (Portrait) moddayız demektir.
+        bool currentIsPortrait = Screen.height > Screen.width;
+
+        // Eğer mod değiştiyse VEYA oyun yeni açıldıysa (_firstRun)
+        if (currentIsPortrait != _lastIsPortrait || _firstRun)
+        {
+            _lastIsPortrait = currentIsPortrait;
+            _firstRun = false;
+
+            // Değişikliği uygula
+            UpdateAllPanels(currentIsPortrait);
+
+            Debug.Log("Ekran Döndü! Yeni Mod: " + (currentIsPortrait ? "Dikey (Portrait)" : "Yatay (Landscape)"));
+        }
+    }
+
+    // Sahnedeki akıllı objeleri bulup günceller
+    void UpdateAllPanels(bool isPortrait)
+    {
+        // Sahnedeki ResponsiveTransform scriptine sahip tüm objeleri bulur (Gizliler dahil)
+        ResponsiveTransform[] allUI = FindObjectsByType<ResponsiveTransform>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (var uiElement in allUI)
+        {
+            uiElement.ApplyOrientation(isPortrait);
+        }
+    }
+}
---
+++ Assets/Scripts/ResponsiveTransform.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResponsiveTransform : MonoBehaviour
+{
+    [System.Serializable]
+    public class LayoutSettings
+    {
+        public Vector2 anchorMin;
+        public Vector2 anchorMax;
+        public Vector2 anchoredPosition;
+        public Vector2 sizeDelta;
+        public Vector3 localScale = Vector3.one;
+        public int gridConstraintCount = 2; // Grid kullanıyorsan sütun sayısı
+    }
+
+    // Editörden ayarları buraya kaydedeceğiz
+    public LayoutSettings landscapeSettings; // Yatay
+    public LayoutSettings portraitSettings;  // Dikey
+
+    private RectTransform rect;
+    private GridLayoutGroup grid;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        grid = GetComponent<GridLayoutGroup>();
+    }
+
+    // OrientationManager tarafından otomatik çağrılır
+    public void ApplyOrientation(bool isPortrait)
+    {
+        if (rect == null) rect = GetComponent<RectTransform>();
+        if (grid == null) grid = GetComponent<GridLayoutGroup>();
+
+        // Hangi ayarı yükleyelim?
+        LayoutSettings target = isPortrait ? portraitSettings : landscapeSettings;
+
+        // Ayarları uygula
+        rect.anchorMin = target.anchorMin;
+        rect.anchorMax = target.anchorMax;
+        rect.anchoredPosition = target.anchoredPosition;
+        rect.sizeDelta = target.sizeDelta;
+        rect.localScale = target.localScale;
+
+        if (grid != null)
+        {
+            grid.constraintCount = target.gridConstraintCount;
+        }
+    }
+
+    // --- SAĞ TIK MENÜSÜ (EDİTÖR İÇİN) ---
+
+    [ContextMenu("Şu anki hali YATAY (Landscape) olarak kaydet")]
+    public void SaveAsLandscape()
+    {
+        rect = GetComponent<RectTransform>();
+        grid = GetComponent<GridLayoutGroup>();
+        landscapeSettings = GetCurrentSettings();
+        Debug.Log(gameObject.name + " için YATAY ayarlar kaydedildi.");
+    }
+
+    [ContextMenu("Şu anki hali DİKEY (Portrait) olarak kaydet")]
+    public void SaveAsPortrait()
+    {
+        rect = GetComponent<RectTransform>();
+        grid = GetComponent<GridLayoutGroup>();
+        portraitSettings = GetCurrentSettings();
+        Debug.Log(gameObject.name + " için DİKEY ayarlar kaydedildi.");
+    }
+
+    private LayoutSettings GetCurrentSettings()
+    {
+        LayoutSettings s = new LayoutSettings();
+        s.anchorMin = rect.anchorMin;
+        s.anchorMax = rect.anchorMax;
+        s.anchoredPosition = rect.anchoredPosition;
+        s.sizeDelta = rect.sizeDelta;
+        s.localScale = rect.localScale;
+        if (grid != null) s.gridConstraintCount = grid.constraintCount;
+        return s;
+    }
+}
